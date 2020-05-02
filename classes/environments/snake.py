5362f7ed7e6cad7336ee2d284c57ae8fd0e0efca
Kevin Haritmonds <haritmonds@yahoo.com>
2020-05-02 22:08:00 +0530

    Add type hints

--- classes/environments/snake.py
+++ classes/environments/snake.py
@@ -1,8 +1,55 @@
 import random
 
 
+class World:
+    DIRECTION_LEFT = (-1, 0)
+    DIRECTION_RIGHT = (1, 0)
+    DIRECTION_UP = (0, -1)
+    DIRECTION_DOWN = (0, 1)
+    ALL_DIRECTIONS = [DIRECTION_LEFT, DIRECTION_RIGHT, DIRECTION_UP, DIRECTION_DOWN]
+
+    CH_EMPTY = ' '
+    CH_OBSTACLE = 'X'
+    CH_SNAKE = '*'
+    CH_SNAKE_HEAD = 'h'
+    CH_FOOD = 'f'
+
+    def __init__(self, board_size: int):
+        # Empty board
+        self.board_size = board_size
+        self.board = [[World.CH_EMPTY for _ in range(board_size)] for _ in range(board_size)]
+        # Borders
+        for y in range(board_size):
+            for x in range(board_size):
+                if x == 0 or y == 0 or x == board_size - 1 or y == board_size - 1:
+                    self.board[y][x] = World.CH_OBSTACLE
+        # Critters
+        self.snake = None
+        self.food = None
+
+    def init_snake(self, pos=None) -> None:
+        self.snake = Snake(self, pos)
+
+    def init_food(self, pos=None) -> None:
+        self.food = Food(self, pos)
+
+    def get_ch(self, pos) -> str:
+        (x, y) = pos
+        return self.board[y][x]
+
+    def put_ch(self, pos, ch) -> None:
+        (x, y) = pos
+        self.board[y][x] = ch
+
+    def draw(self) -> None:
+        for y in range(self.board_size):
+            for x in range(self.board_size):
+                print(self.board[y][x], end='')
+            print()
+
+
 class Snake:
-    def __init__(self, world, pos=None):
+    def __init__(self, world: World, pos=None):
         if pos is None:
             while True:
                 pos = (random.randint(0, world.board_size - 1), random.randint(0, world.board_size - 1))
@@ -10,15 +57,15 @@ class Snake:
                     break
         world.put_ch(pos, World.CH_SNAKE_HEAD)
         self._world: World = world
-        self._snake = [pos]
+        self._snake_pos = [pos]
 
     def head_pos(self) -> (int, int):
-        return self._snake[-1]
+        return self._snake_pos[-1]
 
     def tail_pos(self) -> (int, int):
-        return self._snake[0]
+        return self._snake_pos[0]
 
-    def move(self, direction):
+    def move(self, direction) -> str:
         if direction not in World.ALL_DIRECTIONS:
             raise Exception(f'Invalid direction: {direction}')
         (delta_x, delta_y) = direction
@@ -27,10 +74,10 @@ class Snake:
         ch = self._world.get_ch(new_head_pos)
         if ch == World.CH_EMPTY:
             self._world.put_ch(self.head_pos(), World.CH_SNAKE)
-            self._snake.append(new_head_pos)
+            self._snake_pos.append(new_head_pos)
             self._world.put_ch(self.head_pos(), World.CH_SNAKE_HEAD)
             self._world.put_ch(self.tail_pos(), World.CH_EMPTY)
-            self._snake.pop(0)
+            self._snake_pos.pop(0)
             return ch
         elif ch == World.CH_OBSTACLE:
             return ch
@@ -38,13 +85,13 @@ class Snake:
             return ch
         elif ch == World.CH_FOOD:
             self._world.put_ch(self.head_pos(), World.CH_SNAKE)
-            self._snake.append(new_head_pos)
+            self._snake_pos.append(new_head_pos)
             self._world.put_ch(self.head_pos(), World.CH_SNAKE_HEAD)
             return ch
         else:
             raise Exception(f'Invalid character encountered at pos {new_head_pos}: {ch}')
 
-    def get_visions(self, vision_len):
+    def get_visions(self, vision_len: int):
         snake_head_x, snake_head_y = self.head_pos()
         visions = []
         for y in range(snake_head_y - vision_len, snake_head_y + vision_len + 1):
@@ -60,7 +107,7 @@ class Snake:
 
 
 class Food:
-    def __init__(self, world, pos=None):
+    def __init__(self, world: World, pos=None):
         if pos is None:
             while True:
                 pos = (random.randint(0, world.board_size - 1), random.randint(0, world.board_size - 1))
@@ -69,52 +116,5 @@ class Food:
         world.put_ch(pos, World.CH_FOOD)
         self._food_pos = pos
 
-    def food_pos(self):
+    def food_pos(self) -> (int, int):
         return self._food_pos
-
-
-class World:
-    DIRECTION_LEFT = (-1, 0)
-    DIRECTION_RIGHT = (1, 0)
-    DIRECTION_UP = (0, -1)
-    DIRECTION_DOWN = (0, 1)
-    ALL_DIRECTIONS = [DIRECTION_LEFT, DIRECTION_RIGHT, DIRECTION_UP, DIRECTION_DOWN]
-
-    CH_EMPTY = ' '
-    CH_OBSTACLE = 'X'
-    CH_SNAKE = '*'
-    CH_SNAKE_HEAD = 'h'
-    CH_FOOD = 'f'
-
-    def __init__(self, board_size):
-        # Empty board
-        self.board_size = board_size
-        self.board = [[World.CH_EMPTY for _ in range(board_size)] for _ in range(board_size)]
-        # Borders
-        for y in range(board_size):
-            for x in range(board_size):
-                if x == 0 or y == 0 or x == board_size - 1 or y == board_size - 1:
-                    self.board[y][x] = World.CH_OBSTACLE
-        # Critters
-        self.snake = None
-        self.food = None
-
-    def init_snake(self, pos=None):
-        self.snake = Snake(self, pos)
-
-    def init_food(self, pos=None):
-        self.food = Food(self, pos)
-
-    def get_ch(self, pos) -> str:
-        (x, y) = pos
-        return self.board[y][x]
-
-    def put_ch(self, pos, ch):
-        (x, y) = pos
-        self.board[y][x] = ch
-
-    def draw(self):
-        for y in range(self.board_size):
-            for x in range(self.board_size):
-                print(self.board[y][x], end='')
-            print()
